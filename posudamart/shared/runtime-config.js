decd5d89f5d9cd245a929361c7b8f18b842626bc
aitbayevsaken010-droid <aitbayev.saken010@gmail.com>
2026-03-29 01:07:04 +0500

    Use committed runtime Supabase config with shared client

---
+++ posudamart/shared/runtime-config.js
@@ -0,0 +1,4 @@
+window.__POSUDAMART_RUNTIME_CONFIG__ = Object.freeze({
+  supabaseUrl: 'https://dnpsufzgnudgrqbezioo.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRucHN1ZnpnbnVkZ3JxYmV6aW9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0NjYxNjAsImV4cCI6MjA5MDA0MjE2MH0.N8qP1DfkCmJ9uKMkowtmUYQc-F_ybEyPdfTo86PIGEk',
+});
